32f9bff9044e876563229eb37e00eaa09b758b23
Newdeveloper-c <turdimurodovnumonjon@gmail.com>
2023-08-28 20:59:16 +0500

    migration added

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TelegramBot.Core.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseNpgsql("DataBase");
+});
 
 var app = builder.Build();
 
