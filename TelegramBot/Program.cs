aa774dc59e3396f82789caffc97869c8b1b92a49
Furqat-DevDo <furqata93@gmail.com>
2023-08-29 21:13:31 +0500

    Bot added

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,4 +1,10 @@
+using Telegram.Bot;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var token = builder.Configuration.GetValue("BotKey","");
+
+builder.Services.AddSingleton(new TelegramBotClient(token!));
 
 var app = builder.Build();
 
