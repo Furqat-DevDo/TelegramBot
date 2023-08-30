a0d7beecfa9b890a68d29c6e7675b475153b48b6
Nizomjon <nizomjonvalijonov0302@gmail.com>
2023-08-30 16:28:41 +0500

    Menu va til tanlash deyarli tayyor

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,4 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using TelegramBot.Application.Services;
+using TelegramBot.Application.Services.Handlers;
+using TelegramBot.Core.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var token = builder.Configuration.GetValue("BotKey", "");
+builder.Services.AddSingleton(new TelegramBotClient(token!));
+
+builder.Services.AddSingleton<IUpdateHandler, BotUpdateHandler>();
+
+builder.Services.AddHostedService<BotBackgroundService>();
+
+var connectionString = builder.Configuration.GetConnectionString("UserDb");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
